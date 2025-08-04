874c1e96c148bfe789dac3372a1d3288313e1e08
David Ferreira <davidleandro670@gmail.com>
2025-08-04 11:42:41 -0300

    Arrumar o server

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app')
 
 const PORT = 3000;
 app.listen(PORT, ()=> {
-    console.log(`Conectado servidor em PORT nº'${PORT}`);
+    console.log(`Conectado servidor em PORT nº' ${PORT}`)
 });
