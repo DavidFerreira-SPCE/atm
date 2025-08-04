6b4afb664709fdce85aa20d3195eecc7a19ec41a
David Ferreira <davidleandro670@gmail.com>
2025-08-04 11:31:28 -0300

    Arrumar o app, pois estava funcionando antes de mudar

--- src/app.js
+++ src/app.js
@@ -2,5 +2,9 @@ const express = require('express');
 const app = express();
 const pool = require('./config/db');
 
+const getByIDTransacao = require()
+const getTransacao = require()
+const postTransacao = require()
+
 app.use(express.json());
 module.exports = app
