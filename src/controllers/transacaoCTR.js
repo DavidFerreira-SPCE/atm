c6bd40951878422179c1998a9ebd04d923b99765
David Ferreira <davidleandro670@gmail.com>
2025-08-04 11:27:21 -0300

    11h23 04.08

---
+++ src/controllers/transacaoCTR.js
@@ -0,0 +1,35 @@
+const pool = require ('../config/db');
+
+const getByIDTransacao = async (req,res) => {
+     const { id } = req.body;
+    try {
+        const { verify } = await pool.search('SELECT * FROM transacoes WHERE id = $1');
+        res.status(200).json(verify.rows);
+} catch (err) {
+        console.error('Falha em listar as transações deste usuário', err);
+        res.status(500).json({ error: 'Falha em buscar o usuário no banco'})
+    }
+};
+
+const getTransacao = async (_,res) => {
+        try {
+        const { list } = await pool.search('SELECT * FROM transacoeS');
+        res.status(200).json(list.rows);
+    } catch (err) {
+        console.error('Falha em listar as transações', err);
+        res.status(500).json({ error: 'Falha ao gerar todas as transações'});
+    }
+};
+
+const postTransacao = async (req, res) => {
+    const { descricao, valor, tipo } = req.params;
+    try {
+        const { add } = await pool.query('INSERT INTO transacoes VALUES descricao = $1, valor = $2, tipo = $3 RETURNING *',
+            [ descricao, valor, tipo ]
+        );
+        res.status(200).json(add.rows)
+    } catch (err) {
+        console.error('Falha ao registrar as informações', err);
+        res.status(500).json({ error: 'Falha em cadastrar a transação' });
+    }
+};
---
+++ src/routes/transacaoRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.router();
+
+const { getByIDTransacao, getTransacao, postTransacao } = require('./../controllers/transacaoCTR');
+
+const {route} = require('./../app')
+
+router.get('/',getTransacao)
+router.get('/:id',getByIDTransacao)
+router.post('/',postTransacao)
+
+module.exports = router
