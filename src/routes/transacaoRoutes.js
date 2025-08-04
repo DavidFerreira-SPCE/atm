377d780ecf3fcb90a0950c49f0123cc47d4d8d16
David Ferreira <davidleandro670@gmail.com>
2025-08-04 11:43:00 -0300

    S

--- src/routes/transacaoRoutes.js
+++ src/routes/transacaoRoutes.js
@@ -3,7 +3,7 @@ const router = express.router();
 
 const { getByIDTransacao, getTransacao, postTransacao } = require('./../controllers/transacaoCTR');
 
-const {route} = require('./../app')
+const { route } = require('./../app')
 
 router.get('/',getTransacao)
 router.get('/:id',getByIDTransacao)
