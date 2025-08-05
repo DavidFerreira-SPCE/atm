71264a778e6146898324036a77a09d7136f803f6
David Ferreira <davidleandro670@gmail.com>
2025-08-05 11:17:07 -0300

    Continuar Arrumando By ID

--- src/controllers/transacaoCTR.js
+++ src/controllers/transacaoCTR.js
@@ -1,19 +1,26 @@
 const pool = require('../config/db.js');
 
-const getByIDTransacao = async (req,res) => {
-     const { id } = req.params;
-    try {
-        const verify  = await pool.search('SELECT * FROM transacoes WHERE id = $1');
-        res.status(200).json(verify.rows);
-} catch (err) {
-        console.error('Falha em listar as transações deste usuário', err);
-        res.status(500).json({ error: 'Falha em buscar o usuário no banco'})
+
+const getByIDTransacao = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const verify = await pool.query('SELECT * FROM transacoes WHERE id = $1', [id]);
+
+    if (verify.rows.length === 0) {
+      return res.status(404).json({ mensagem: 'Transação não encontrada.' });
     }
+
+    res.status(200).json(verify.rows[0]);
+  } catch (err) {
+    console.error('Falha ao listar a transação', err);
+    return res.status(500).json({ error: 'Falha ao buscar a transação no banco.' });
+  }
 };
 
 const getTransacao = async (_,res) => {
         try {
-        const list = await pool.search('SELECT * FROM transacoes');
+        const list = await pool.query('SELECT * FROM transacoes');
         res.status(200).json(list.rows);
     } catch (err) {
         console.error('Falha em listar as transações', err);
@@ -36,7 +43,7 @@ const postTransacao = async (req, res) => {
         const  add  = await pool.query('INSERT INTO transacoes (descricao, valor, tipo) VALUES ($1, $2, $3) RETURNING *',
             [ descricao, valor, tipo ]
         );
-        res.status(200).json(add.rows)
+        res.status(200).json(add.rows[0])
     } catch (err) {
         console.error('Falha ao registrar as informações', err);
         res.status(500).json({ error: 'Falha em cadastrar a transação' });
--- src/routes/transacaoRoutes.js
+++ src/routes/transacaoRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router();
 
+
+
 const { getByIDTransacao, getTransacao, postTransacao } = require('./../controllers/transacaoCTR');
 
 router.get('/',getTransacao)
