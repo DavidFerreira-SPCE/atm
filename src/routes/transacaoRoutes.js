69bdf7d2972ec259ee19d7d5211b2e55905f8411
David Ferreira <davidleandro670@gmail.com>
2025-08-05 08:50:07 -0300

    minor error on line6 with route const.

--- src/routes/transacaoRoutes.js
+++ src/routes/transacaoRoutes.js
@@ -3,8 +3,6 @@ const router = express.router();
 
 const { getByIDTransacao, getTransacao, postTransacao } = require('./../controllers/transacaoCTR');
 
-const { route } = require('./../app')
-
 router.get('/',getTransacao)
 router.get('/:id',getByIDTransacao)
 router.post('/',postTransacao)
