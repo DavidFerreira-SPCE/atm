a0ff9e42140df636cec7d92e746575a54b871fa3
David Ferreira <davidleandro670@gmail.com>
2025-08-06 09:30:54 -0300

    minor fix

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,15 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'atm',
-    password:'wcc@2023',
+    user: 'postgres',
+    host: 'localhost',
+    database: 'atm',
+    password: 'wcc@2023',
     port: 5432,
 });
 
 pool.connect()
     .then(() => console.log('Connectado ao Banco...'))
-    .catch(err => console.error('Algo deu errado, tente novamente...',err));
+    .catch(err => console.error('Algo deu errado, tente novamente...', err));
 
 module.exports = pool;
